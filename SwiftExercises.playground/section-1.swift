13cb9f3ac30fc0cda28e77d46cb300643be498cf
Reece Webb <inbox@reece.org>
2015-02-08 01:26:38 -0800

    Completed swift vs objective-c

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -8,25 +8,26 @@ Strings
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
     // WORK HERE
-    return cheese
+    return "My favorite cheese is " + cheese
 }
 
-let fullSentence = favoriteCheeseStringWithCheese("cheddar")
 // Make fullSentence say "My favorite cheese is cheddar."
-
+let fullSentence = favoriteCheeseStringWithCheese("cheddar")
 /*
 
 Arrays & Dictionaries
 
 */
 
-let numberArray = [1, 2, 3, 4]
+var numberArray = [1, 2, 3, 4]
 // Add 5 to this array
 // WORK HERE
+numberArray.append(5)
 
-let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+var numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
 // WORK HERE
+numberDictionary[5] = "five"
 
 /*
 
@@ -36,9 +37,15 @@ Loops
 
 // Use a closed range loop to print 1 - 10, inclusively
 // WORK HERE
+for closedLoop in 1...10 {
+    println(closedLoop)
+}
 
 // Use a half-closed range loop to print 1 - 10, inclusively
 // WORK HERE
+for halfLoop in 1..<10 {
+    println(halfLoop)
+}
 
 let worf = [
     "name": "Worf",
@@ -58,12 +65,18 @@ let characters = [worf, picard]
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
     // WORK HERE
-    return []
+    var drinkArray = [String]()
+    for character in characters {
+        if let drink = character["favorite drink"] {
+            drinkArray.append(drink)
+        }
+    }
+
+    return drinkArray
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
 
-favoriteDrinks
 
 /*
 
@@ -76,8 +89,12 @@ Functions
 let strings = ["milk", "eggs", "bread", "challah"]
 
 // WORK HERE - make your function and pass `strings` in
+func array2String(strings:Array<String>) -> String {
+    let joinedString = ";".join(strings)
+    return joinedString
+}
 
-let expectedOutput = "milk;eggs;bread;challah"
+let expectedOutput = array2String(strings)
 
 /*
 
@@ -89,3 +106,4 @@ let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+let cerealSorted = sorted(cerealArray, <)
